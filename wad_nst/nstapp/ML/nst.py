e87d0a613ee7f96e9f43563cf887da51f1b32c57
shreyasms <shreyasms@phaidra.ai>
2021-05-06 12:48:12 +0530

    Add documentation for img_unloader.py and nst.py

--- wad_nst/nstapp/ML/nst.py
+++ wad_nst/nstapp/ML/nst.py
@@ -2,40 +2,114 @@ from . import *
 
 
 class ContentLoss(nn.Module):
+    """Class to implement loss for content image
+
+    Parameters
+    ----------
+    target : torch.Tensor
+        Target on which loss is calculated.
+    """
+
     def __init__(self, target):
         super(ContentLoss, self).__init__()
         self.target = target.detach()
 
     def forward(self, input):
+        """Forward function
+
+        Parameters
+        ----------
+        input : torch.Tensor
+            Used to calculate Loss
+
+        Returns
+        -------
+        input: torch.Tensor
+            Returns the same input.
+        """
         self.loss = F.mse_loss(input, self.target)
         return input
 
 
 class StyleLoss(nn.Module):
+    """Class to implement loss for style image.
+
+    Parameters
+    ----------
+    input: torch.Tensor
+        the input that is later reshaped.
+    """
+
+    def __init__(self, target_feature):
+        super(StyleLoss, self).__init__()
+        self.target = self.gram_matrix(target_feature).detach()
+
     def gram_matrix(self, input):
+        """Calculate the gram matrix.
+
+        Parameters
+        ----------
+        input : torch.Tensor
+            The input tensor.
+
+        Returns
+        -------
+        gm: torch.Tensor
+            returns the gram matrix.
+        """
         a, b, c, d = input.size()
         features = input.view(a * b, c * d)
         G = torch.mm(features, features.t())
 
         return G.div(a * b * c * d)
 
-    def __init__(self, target_feature):
-        super(StyleLoss, self).__init__()
-        self.target = self.gram_matrix(target_feature).detach()
-
     def forward(self, input):
+        """Forward function
+
+        Parameters
+        ----------
+        input : torch.Tensor
+            Used to calculate loss
+
+        Returns
+        -------
+        input: torch.Tensor
+            Returns the same input.
+        """
         G = self.gram_matrix(input)
         self.loss = F.mse_loss(G, self.target)
         return input
 
 
 class Normalization(nn.Module):
+    """Normalization helper class
+
+    Parameters
+    ----------
+    mean : float
+        The mean of the data.
+    std: float
+        The standard deviation of the data.
+    """
+
     def __init__(self, mean, std):
         super(Normalization, self).__init__()
         self.mean = torch.tensor(mean).view(-1, 1, 1)
         self.std = torch.tensor(std).view(-1, 1, 1)
 
     def forward(self, img):
+        """The forward function.
+
+        Parameters
+        ----------
+        img : torch.Tensor
+            The input data to be normalized.
+
+        Returns
+        -------
+        img: torch.Tensor
+            The normalized image.
+        """
         return (img - self.mean) / (self.std)
 
 
@@ -48,6 +122,26 @@ def get_style_model_and_losses(
     content_layers=content_layers_default,
     style_layers=style_layers_default,
 ):
+    """This function is used to get the style and content losses for the model.
+
+    Parameters
+    ----------
+    cnn : nn.Module
+        The convolutional neural network model.
+    normalization_mean : float
+        The mean to use for normalization.
+    normalization_std : float
+        The standard deviation for the normalization.
+    style_img : torch.Tensor
+        The style image.
+    content_img : torch.Tensor
+        The content image.
+
+    Returns
+    -------
+    tuple
+        tuple of model, style loss and content loss.
+    """
     cnn = copy.deepcopy(cnn)
 
     normalization = Normalization(normalization_mean, normalization_std).to(device)
@@ -96,5 +190,17 @@ def get_style_model_and_losses(
 
 
 def get_input_optimizer(input_img):
+    """This function initializes and returns the optimizer.
+
+    Parameters
+    ----------
+    input_img : torch.Tensor
+        input image.
+
+    Returns
+    -------
+    optimizer
+        torch.optim
+    """
     optimizer = optim.LBFGS([input_img.requires_grad_()])
     return optimizer
